2d880c2a5e07b05cd0a96f6b2a09d2b24d1d7fb5
Kthommd <kgtrocks@gmail.com>
2025-08-08 14:03:11 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+:
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Vite configuration for React and Tailwind
+export default defineConfig({
+  plugins: [react()],
+});
